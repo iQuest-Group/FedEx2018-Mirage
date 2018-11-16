1a27ab6c4f034e325c3e15b3f4cf19d4227fbe94
Adrian Gogolan <adrian.gogolan@iquestgroup.com>
2018-11-16 10:42:22 +0200

    Added input and output.

--- chroma_compare.py
+++ chroma_compare.py
@@ -5,11 +5,14 @@ Created on Mon Nov  5 14:34:53 2018
 @author: adrian.gogolan
 """
 
-import numpy, scipy, matplotlib.pyplot as plt, IPython.display as ipd
+import matplotlib.pyplot as plt, IPython.display as ipd
 import librosa, librosa.display
 import stanford_mir; stanford_mir.init()
 
-x, sr = librosa.load('audio/simple_piano.wav')
+audio_file = input('audio file:')
+
+#x, sr = librosa.load('audio/simple_piano.wav')
+x, sr = librosa.load(audio_file)
 ipd.Audio(x, rate=sr)
 
 fmin = librosa.midi_to_hz(36)
@@ -17,8 +20,10 @@ hop_length = 512
 C = librosa.cqt(x, sr=sr, fmin=fmin, n_bins=72, hop_length=hop_length)
 
 chromagram = librosa.feature.chroma_stft(x, sr=sr, hop_length=hop_length)
-plt.figure(figsize=(15, 5))
-librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=hop_length, cmap='coolwarm')
+#plt.figure(figsize=(15, 5))
+#librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length=hop_length, cmap='coolwarm')
 
+print(chromagram.tolist())
 
-print (chromagram[0,0])
+#with open('output', 'w') as f:
+ #  numpy.savetxt(f, chromagram)
