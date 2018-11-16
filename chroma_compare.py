0e59f6a1f7fae9a2690bd0817fa028c6e3cd6864
Radu Soare <Radu.Soare@iquestgroup.com>
2018-11-16 11:19:33 +0200

    Added commented argument parser

--- chroma_compare.py
+++ chroma_compare.py
@@ -8,8 +8,16 @@ Created on Mon Nov  5 14:34:53 2018
 import matplotlib.pyplot as plt, IPython.display as ipd
 import librosa, librosa.display
 import stanford_mir; stanford_mir.init()
+import argparse
 
+#parser = argparse.ArgumentParser(description="Sets up the build by creating the necessary configuration files", epilog="")
+#parser.add_argument('--sourceFile', '-s', help="path to folder containing the configuration files to be copied", required=False, default="audio/simple_piano.wav")
+#arguments = parser.parse_args()
+
+#audio_file = arguments.sourceFile
+#audio_file = input('audio/simple_piano.wav')
 audio_file = input('audio file:')
+
 
 #x, sr = librosa.load('audio/simple_piano.wav')
 x, sr = librosa.load(audio_file)
